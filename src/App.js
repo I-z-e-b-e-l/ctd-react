e84b50e009ccb324a742a2d5caf4ff09d053f2a0
izebel <izebel@rocketmail.com>
2022-11-17 14:24:03 -0500

    new state todolist in App, passed to ToDoList as prop. hardcoded todoList array removed

--- src/App.js
+++ src/App.js
@@ -6,13 +6,15 @@ import AddToDoForm from './AddToDoForm.js';
 function App() {
 
   const [newTodo, setNewTodo] = React.useState("");
+
+  const [todoListstate, setTodoListstate] = React.useState([]);
   
   return (
     <div>
       <h1>To Do List</h1>
       <AddToDoForm onAddTodo={setNewTodo} />
       <p>You're adding:{newTodo}</p>
-      <ToDoList />
+      <ToDoList todoListstate={todoListstate}/>
       
     </div>
   );
@@ -20,8 +22,14 @@ function App() {
 
 export default App;
 
-
-
+// ☑ Create new state variable named todoList with setter setTodoList and default value of an empty Array
+// ☑  Pass todoList state as a prop named todoList to the TodoList component
+// In /src/TodoList.js
+    // ☑ Add props as a parameter to the TodoList functional component
+    // Change todoList to reference props instead of the hard-coded variable
+    // Delete the hard-coded todoList variable
+    // Run your application and view in browser
+    // Verify that your Todo List is now empty (no list items)
 
 
 
--- src/TodoList.js
+++ src/TodoList.js
@@ -2,32 +2,35 @@ import React from "react";
 import TodoListItem from "./TodoListItem";
 
 
-  const todoList = [
-      {
-        id: 1,
-        title: "wash face"
-      },
-      {
-        id: 2,
-        title: "make coffee"
-      },
-      {
-        id: 3,
-        title: "physical therapy"
-      }
-    ];
+  // const todoList = [
+  //     {
+  //       id: 1,
+  //       title: "wash face"
+  //     },
+  //     {
+  //       id: 2,
+  //       title: "make coffee"
+  //     },
+  //     {
+  //       id: 3,
+  //       title: "physical therapy"
+  //     }
+  //   ];
 
-function ToDoList() {
+function ToDoList(props) {
 
     return (
+        <div>
+        <h2>My to do list:</h2>
         <ul>
-          {todoList.map(function (item) {
+          {props.todoListstate.map(function (item) {
             return (
               <TodoListItem key={item.id} arrayitem={item}/>
  
             );
           })}
         </ul>
+        </div>
     );
   };
 
