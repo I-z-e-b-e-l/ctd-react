07a1abec0329c945f28ac37e1e06b325df80f808
izebel <izebel@rocketmail.com>
2022-11-26 14:27:44 -0500

    finish section Control Add Todo Input

--- src/AddToDoForm.js
+++ src/AddToDoForm.js
@@ -2,24 +2,42 @@ import React from "react";
 
 function AddToDoForm(props){
 
+    const [todoTitle, setTodoTitle] = React.useState("");
+
+    const handleTitleChange = (event) => {
+        let newTodoTitle = event.target.value;
+        setTodoTitle(newTodoTitle);
+    };
+
     const handleAddTodo = (event) => {
         event.preventDefault();
-        const todoTitle = event.target.title.value;
-        console.log(todoTitle);
-
         props.onAddTodo(todoTitle);
+        event.target.reset("");
+        // console.log(event.target.value)
+    };
 
-        event.target.reset();
-
-    }
+    // const handleAddTodo = (event) => {
+    //     event.preventDefault();
+    //     const todoTitle = event.target.title.value;
+    //     console.log(todoTitle);
+    //     props.onAddTodo(todoTitle);
+    //     event.target.reset();
+    // };
 
     return(
-        <form onSubmit={handleAddTodo} onAddTodo>
+        <form onSubmit={handleAddTodo}>
             <label htmlFor='todoTitle'>title</label>
-            <input id='todoTitle' type="text" name="title" />
+            <input 
+                id='todoTitle' 
+                type="text" 
+                name="title"
+                value={todoTitle}
+                onChange={handleTitleChange} />
             <button type='submit'>Add</button>
         </form>
     )
 }
 
 export default AddToDoForm
+
+// ☑ 
--- src/App.js
+++ src/App.js
@@ -9,27 +9,23 @@ function App() {
 
   const [todoListstate, setTodoListstate] = React.useState([]);
   
+  // const handleOnAddToDo = (value) => {
+  //   setNewTodo(value)
+  // }
+
   return (
     <div>
       <h1>To Do List</h1>
-      <AddToDoForm onAddTodo={setNewTodo} />
+      {/* <AddToDoForm onAddTodo={handleOnAddToDo} newTodo={newTodo}/> */}
+      <AddToDoForm onAddTodo={setNewTodo} newTodo={newTodo}/>
       <p>You're adding:{newTodo}</p>
-      <ToDoList todoListstate={todoListstate}/>
+      <ToDoList todoListstate={todoListstate} />
       
     </div>
   );
 };
 
 export default App;
-
-// ☑ Create new state variable named todoList with setter setTodoList and default value of an empty Array
-// ☑  Pass todoList state as a prop named todoList to the TodoList component
-// In /src/TodoList.js
-    // ☑ Add props as a parameter to the TodoList functional component
-    // Change todoList to reference props instead of the hard-coded variable
-    // Delete the hard-coded todoList variable
-    // Run your application and view in browser
-    // Verify that your Todo List is now empty (no list items)
 
 
 
