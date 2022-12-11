e4c9674d212ab2cf209671537d56f1d5e1c8fa67
izebel <izebel@rocketmail.com>
2022-12-11 11:30:12 -0500

    remove custom hook: useSemiPersistentState

--- src/App.js
+++ src/App.js
@@ -3,7 +3,20 @@ import ToDoList from './TodoList.js';
 import AddToDoForm from './AddToDoForm.js';
 // import TodoListItem from './TodoListItem.js';
 
-const useSemiPersistentState = () => {
+// const useSemiPersistentState = () => {
+
+//   const [todoListState, setTodoListState] = React.useState(JSON.parse(localStorage.getItem('savedTodoList')) || []);
+
+//   React.useEffect(()=> {
+//     localStorage.setItem('savedTodoList', JSON.stringify(todoListState))
+//   },[todoListState]);
+
+//   return [todoListState, setTodoListState];
+// }
+
+function App() {
+
+  // const [todoListState, setTodoListState] = useSemiPersistentState()
 
   const [todoListState, setTodoListState] = React.useState(JSON.parse(localStorage.getItem('savedTodoList')) || []);
 
@@ -11,20 +24,13 @@ const useSemiPersistentState = () => {
     localStorage.setItem('savedTodoList', JSON.stringify(todoListState))
   },[todoListState]);
 
-  return [todoListState, setTodoListState];
-}
 
-function App() {
-
-  const [todoListState, setTodoListState] = useSemiPersistentState()
 
   //overwrite the todoListState array with a new one that includes newTodo
   const addTodo = (newTodo) => {
     setTodoListState([...todoListState, newTodo]);
   }
 
-
-//The logic here should be correct, so I'm assuming there's something elsewhere in the application that's hanging things up
 
   const removeTodo = id => {
     console.log(id)
@@ -33,15 +39,6 @@ function App() {
     );
     setTodoListState(newTodoList)
   }
-
-
-  // This works. I can't get the above to run taking in just id as a parameter. 
-  // const removeTodo = item => {
-  //   const newTodoList = todoListState.filter(
-  //     todo => item.id !== todo.id
-  //   );
-  //   setTodoListState(newTodoList)
-  // }
 
 
   return (
